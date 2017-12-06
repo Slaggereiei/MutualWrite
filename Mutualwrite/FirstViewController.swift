638ae31ac7eefe7fcb35dda5901235fbbd8e6f66
Sritongsuk <nattakrit12@hotmail.com>
2017-12-06 22:34:06 +0700

    Initial Commit

---
+++ Mutualwrite/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mutualwrite
+//
+//  Created by Sritongsuk on 6/12/2560 BE.
+//  Copyright © 2560 Sritongsuk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
